361146f80b117a4f606d9437d2bea81958f0fa4c
OmbhagwanIos <iosdaveloperom@gmail.com>
2023-11-18 22:31:23 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by iOS Cat on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
